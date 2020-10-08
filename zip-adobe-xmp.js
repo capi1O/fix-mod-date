f6206c5409aa2bc45f1166abf361e9f5dc4eb3d4
didrip <dri@mess.ms>
2020-10-08 18:51:32 +0200

    workaround for prproj

--- zip-adobe-xmp.js
+++ zip-adobe-xmp.js
@@ -1,5 +1,6 @@
 // unzip and search for <MZ.BuildVersion.Modified>
 const zlib	= require('zlib');
+const xmp		= require('./adobe-xmp');
 
 
 const mtime = async (zippedData, verbose, absFilePath) => {
@@ -8,17 +9,25 @@ const mtime = async (zippedData, verbose, absFilePath) => {
 		let timestamp = null;
 
 		// 1. unzip
-		const data = zlib.gunzipSync(zippedData); //, { windowBits });
+		const dataString = zlib.gunzipSync(zippedData).toString('utf8');
 
 
-		// 2. find <WorkspaceDefinition> section
-		const match = data.toString('utf8').match(/<MZ\.BuildVersion\.Modified>(.*)<\/MZ\.BuildVersion\.Modified>/m); // 14.0.1x71 - 01-Oct-20 16:18:57
-		if (match === null) console.error(`could not find <MZ.BuildVersion.Modified> in ${absFilePath}`);
-		else {
-			const dateString = match[1]; // HTML encoded content
-			if (verbose) console.log(`build date from ${absFilePath} is '${dateString}'`);
-			// const date = new Date(dateString);
-			// timestamp = date.getTime();
+		// 2. try to get Adobe XMP date
+		timestamp = await xmp.mtime(dataString, verbose, absFilePath);
+		// console.log(timestamp);
+
+		// 3. if not found read build find <WorkspaceDefinition> which is the build date of the Premiere software file was made with, therefore file can only be older
+		if (!timestamp) {
+			const match = dataString.match(/<MZ\.BuildVersion\.Modified>.*\s-\s(.*)<\/MZ\.BuildVersion\.Modified>/m); // ex 01-Oct-20 16:18:57 or 9/24/2018 2:19:21 PM
+			if (match === null) console.error(`could not find <MZ.BuildVersion.Modified> in ${absFilePath}`);
+			else {
+				const dateString = match[1];
+				if (verbose) console.log(`build date from ${absFilePath} is '${dateString}'`);
+				const date = new Date(dateString);
+				timestamp = date.getTime();
+
+				// TODO : only modify date if current modified date is earlier than build date
+			}
 		}
 
 		return timestamp;
